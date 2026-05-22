7229e74a92a819e025d4fecb47cd0bc55ab2e088
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2026-05-22 14:16:27 +0200

    Add files via upload

---
+++ DisplayingTheMultiplicationOfTwoNumbers/src/main/java/com/mycompany/displayingthemultiplicationoftwonumbers/DisplayingTheMultiplicationOfTwoNumbers.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.displayingthemultiplicationoftwonumbers;
+
+/**
+ *
+ * @author moka
+ */
+// Write A Java Program To Get The Multiplication Of The Two Numbers =>
+public class DisplayingTheMultiplicationOfTwoNumbers{
+    public static void main(String[] args){
+    int fristNum = 20;
+    System.out.println("The Frist Number Is = "+fristNum);
+    int secondNum = 30;
+    System.out.println("The Second Number Is = "+secondNum);
+    int multiplicationOfTwoNumbers = fristNum * secondNum;
+    System.out.println("The Multiplication Of The Two Numbers Is = "+multiplicationOfTwoNumbers);
+    }
+}
