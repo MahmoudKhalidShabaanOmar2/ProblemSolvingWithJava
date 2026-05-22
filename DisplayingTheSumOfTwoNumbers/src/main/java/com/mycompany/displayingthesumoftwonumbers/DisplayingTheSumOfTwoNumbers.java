ad19c7d53f7a30f3c973e509cc7fae31cf46ec9e
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2026-05-22 14:06:11 +0200

    Add files via upload

---
+++ DisplayingTheSumOfTwoNumbers/src/main/java/com/mycompany/displayingthesumoftwonumbers/DisplayingTheSumOfTwoNumbers.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.displayingthesumoftwonumbers;
+
+/**
+ *
+ * @author moka
+ */
+// Write A Java Program To Display The Sum Of The Two Integer Numbers =>
+public class DisplayingTheSumOfTwoNumbers{
+    public static void main(String[] args){
+    int fristNum = 34;
+    System.out.println("The Frist Number Is = "+fristNum);
+    int secondNum = 23;
+    System.out.println("The Second Number Is = "+secondNum);
+    int sumOfTwoNumbers = fristNum + secondNum;
+    System.out.println("The Sum Of The Two Numbers Is = "+sumOfTwoNumbers);
+    }
+}
