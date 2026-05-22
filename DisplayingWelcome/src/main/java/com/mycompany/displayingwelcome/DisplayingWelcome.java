fe0d71b04200413262cb482a47a77d44e99dc9c8
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2026-05-22 13:58:54 +0200

    Add files via upload

---
+++ DisplayingWelcome/src/main/java/com/mycompany/displayingwelcome/DisplayingWelcome.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.displayingwelcome;
+
+/**
+ *
+ * @author moka
+ */
+// Write A Java Program To Display Welcome Message For Different Users With The User Name =>
+public class DisplayingWelcome{
+    public static void main(String[] args){
+    String userName = "Mahmoud Khalid";
+    System.out.println("The User Name Is : "+userName);
+    System.out.println("Welcome "+userName);
+    }
+}
