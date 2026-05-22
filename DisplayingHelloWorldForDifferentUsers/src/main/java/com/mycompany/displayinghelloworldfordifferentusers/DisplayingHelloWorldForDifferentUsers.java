d09979055550760c0b652055c1717badd5aeb353
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2026-05-22 13:49:05 +0200

    Add files via upload

---
+++ DisplayingHelloWorldForDifferentUsers/src/main/java/com/mycompany/displayinghelloworldfordifferentusers/DisplayingHelloWorldForDifferentUsers.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.displayinghelloworldfordifferentusers;
+
+/**
+ *
+ * @author moka
+ */
+public class DisplayingHelloWorldForDifferentUsers{
+    public static void main(String[] args){
+    System.out.println("Hello World , And Welcome From Java");
+    }
+}
