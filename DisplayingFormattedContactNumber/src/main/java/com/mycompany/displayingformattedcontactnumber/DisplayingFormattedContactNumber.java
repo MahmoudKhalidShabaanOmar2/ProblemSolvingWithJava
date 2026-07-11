69ec113702c32f855d3181d8ba30d5800d2d19a2
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2026-07-11 23:26:14 +0200

    Add files via upload

---
+++ DisplayingFormattedContactNumber/src/main/java/com/mycompany/displayingformattedcontactnumber/DisplayingFormattedContactNumber.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.displayingformattedcontactnumber;
+
+/**
+ *
+ * @author moka
+ */
+// Write A Java Program To Get Array Of Ten Numbers From Zero To Nine , And Then 
+// Displaying The Phone Number From This Ten Numbers By Using Printf() Method =>
